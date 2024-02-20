7477974aca4203ca0cf2fef961b9160829cba579
Williams Hernández <hernandw@gmail.com>
2024-02-20 08:47:55 -0300

    :bug: menu hamburguesa

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+$(document).ready(() => {
+  $(".navbar-nav>li>a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+})
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
